a4eeee4d0d0ad1ab7f003e1750a48ebadb2c0740
Murat Kizilay <mail.muratkizilay@gmail.com>
2019-12-26 19:08:17 +0300

    Day 3(Watermelon).

---
+++ IntroCompetitiveProgrammingCodes/Day 03/Watermelon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntroCompetitiveProgrammingCodes.Day_03
+{
+    class Watermelon
+    {
+        static void Main(string[] args)
+        {
+            int w = Convert.ToInt32(Console.ReadLine());
+
+            if (w % 2 == 0)
+            {
+                if (w != 2)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
